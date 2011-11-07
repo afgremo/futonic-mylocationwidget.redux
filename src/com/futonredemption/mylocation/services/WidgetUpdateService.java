503f07b35004219157e8cc6e4432df7ffc0061dd
futonredemption <futonredemption@localhost>
2011-11-06 21:53:27 -0500

    Changes

--- src/com/futonredemption/mylocation/services/WidgetUpdateService.java
+++ src/com/futonredemption/mylocation/services/WidgetUpdateService.java
@@ -3,7 +3,6 @@ package com.futonredemption.mylocation.services;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 
 import org.beryl.app.AbstractService;
 import org.beryl.diagnostics.Logger;
